115655a542355b058188281fdf8c0f01aa40d52f
kscold <201921339@sangmyung.kr>
2024-04-03 17:12:07 +0900

    #2 - 리액트 input 이벤트에 대해서 실습
    리액트 input 이벤트에 대해서 실습하고 렌더링함

--- likelion/src/App.js
+++ likelion/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState("")
+
+  const onChnage = (e) => setInput(e.target.value)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <input
+        onChange={onChnage}
+        placeholder="문자를 입력해주세요"
+        value={input}
+      />
+      {input}
+    </>
+  )
 }
 
-export default App;
+export default App
